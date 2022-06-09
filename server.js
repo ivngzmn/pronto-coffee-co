a895a016c6dfb3aee5ebfc646e9ef4a95e891aff
Ivan Guzman <hello@ivanguzman.dev>
2022-06-08 22:21:33 -0700

    refactor(objectId): refactored code to update deprecated ObjectID to ObjectId

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const session = require("express-session");
-const passport = require("passport");
-const flash = require("connect-flash");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const configDB = require('./config/database.js');
+const ObjectId = require('mongodb').ObjectId;
 
-const configDB = require("./config/database.js");
-
-require("dotenv").config({ path: "./config/.env" });
-const ObjectId = require("mongodb").ObjectID;
+require('dotenv').config({ path: './config/.env' });
 
 let db;
 
 mongoose.connect(configDB.url, (err, database) => {
   if (err) return console.log(err);
   db = database;
-  require("./app/routes.js")(app, passport, db, ObjectId);
+  require('./app/routes.js')(app, passport, db, ObjectId);
 });
 
-require("./config/passport")(passport);
-app.use(morgan("dev"));
+require('./config/passport')(passport);
+app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(
   session({
-    secret: "keyboard cat bongo",
+    secret: 'keyboard cat bongo',
     resave: true,
     saveUninitialized: true,
     cookie: { maxAge: 1000 * 60 * 60 * 24 }, // Cookie Life - 24 hours
@@ -44,7 +43,7 @@ app.use(flash());
 
 app.listen(process.env.PORT || 3000, function () {
   console.log(
-    "The server is listening on port %d in %s mode",
+    'The server is listening on port %d in %s mode',
     this.address().port,
     app.settings.env
   );
