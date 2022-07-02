0480dea361c5e0df6aa9bb7ff4cb16786c7c2fbb
Ivan Guzman <hello@ivanguzman.dev>
2022-07-02 01:14:30 -0700

    updated server comments

--- server.js
+++ server.js
@@ -1,39 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const session = require('express-session');
-const passport = require('passport');
-const flash = require('connect-flash');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const ObjectId = require('mongodb').ObjectId;
-const configDB = require('./config/database.js');
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const flash = require("connect-flash");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const ObjectId = require("mongodb").ObjectId;
+const configDB = require("./config/database.js");
+const PORT = 3000;
 
-require('dotenv').config({ path: './config/.env' });
+require("dotenv").config({ path: "./config/.env" });
 
 mongoose.connect(configDB.url, (err, database) => {
   if (err) return console.log(err);
-  console.log('Connected to database');
+  console.log(`Connected to ${configDB.dbName} DB 🗃'`);
   let db = database;
   // routes
-  require('./app/routes.js')(app, passport, db, ObjectId);
+  require("./app/routes.js")(app, passport, db, ObjectId);
 }); // connect to our database
 
-require('./config/passport')(passport); // pass passport for configuration
+require("./config/passport")(passport); // pass passport for configuration
 // set up express application
-app.use(morgan('dev')); // log every request to the console
+app.use(morgan("dev")); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // required for passport
 app.use(
   session({
-    secret: 'keyboard cat bongo',
+    secret: "keyboard cat bongo",
     resave: true,
     saveUninitialized: true,
     cookie: { maxAge: 1000 * 60 * 60 * 24 }, // Cookie Life - 24 hours
@@ -44,6 +45,8 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // launch 🚀
-app.listen(process.env.PORT || 3000, function () {
-  console.log('The server is listening on port %d in %s mode');
+app.listen(process.env.PORT || PORT, function () {
+  console.log(
+    `🚨 The server is listening on port ${PORT} you better go catch it!!! 🚨`
+  );
 });
